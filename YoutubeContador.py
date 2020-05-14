5fd836cb68b95b58f20c6b1c331a0e6782161270
Nickolas Pogozelski <65191137+NickoalsPogozelski@users.noreply.github.com>
2020-05-14 16:41:19 -0300

    Add files via upload
    Adicionado o script

---
+++ YoutubeContador.py
@@ -0,0 +1,30 @@
+import urllib.request
+import json
+import keyboard
+from playsound import playsound
+import time
+
+OldSubCount = '0'
+
+def subscribers():
+    global OldSubCount
+
+    data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&id={}&key={}").read() #substitua as chaves pelo id do seu canal e sua key
+    subs = (json.loads(data)["items"][0]["statistics"]["subscriberCount"])
+
+    if OldSubCount == '0':
+        OldSubCount = subs
+
+    if subs > OldSubCount:       
+        playsound('{}') #nas chaves você bota o caminho para o arquivo de som
+        OldSubCount = subs
+        
+    print(subs)
+
+
+while True:
+    time.sleep(3) #add um delay de 3 segundos
+    subscribers()
+    
+    if keyboard.is_pressed('esc'):
+        break
